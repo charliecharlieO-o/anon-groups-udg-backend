2d39ffd834e9e4e4156d9361b9544f036b476c5f
Charles <cgarzon132@gmail.com>
2017-08-09 00:16:11 -0500

    Development safe XSS enabled, must disable in the future

--- app.js
+++ app.js
@@ -49,6 +49,13 @@ mongoose.connect(config.database, (err, res) => {
   }
 });
 
+/* DEV ONLY*/
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //app.use("/", index);
 app.use("/user", users);
 app.use("/board", boards);
