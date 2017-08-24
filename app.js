4ba9b6ecaad3efb5a02e0af260ea5472b99bb2ae
Charles <cgarzon132@gmail.com>
2017-08-23 19:40:18 -0500

    Added temporary CORS support

--- app.js
+++ app.js
@@ -52,8 +52,15 @@ mongoose.connect(config.database, (err, res) => {
 /* DEV ONLY*/
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.sendStatus(200);
+  }
+  else {
+    next();
+  }
 });
 
 //app.use("/", index);
