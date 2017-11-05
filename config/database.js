b1a5866a2a0550015c88a0e832b5439e55e6b176
Victor Diaz <mars.vic5@gmail.com>
2017-11-05 02:07:51 -0600

    config for mongoproduction

--- config/database.js
+++ config/database.js
@@ -1,4 +1,22 @@
+// mongo server host: 198.199.79.16
+const DBHOST = '198.199.79.16'
+
+// USER
+const USER = 'maskmobproductionuser'
+const PASSWORD = '7654321'
+const AUTH = `${USER}:${PASSWORD}`
+
+// DB prduction name: maskmobproduction
+const DBNAME = 'maskmobproduction'
+
+// DB production port: 2018
+const PORT = '2018'
+
+const CONNECT_URI = `mongodb://${AUTH}@${DBHOST}:${PORT}/${DBNAME}`
+
+console.log('connect uri', CONNECT_URI)
+
 module.exports = {
 	"secret":"5edc4e87b66c1HellSpawner10a6e2aad057df237bb", // Change to something more secret
-	"database":"mongodb://localhost/maskmob"
-};
+	database: CONNECT_URI
+}
