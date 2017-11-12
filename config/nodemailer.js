8ccdf8879410f87de897e8a219dbfe1a4e7c07d7
Charles <cgarzon132@gmail.com>
2017-11-12 00:14:45 -0600

    Added mailgun mg.netsla.me settings

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,16 +1,16 @@
 const nodemailer = require('nodemailer')
+const mg = require('nodemailer-mailgun-transport')
 
 module.exports = {
   async sendMail (mail) {
     try {
-      const transport = await nodemailer.createTransport({
-          host: 'smtp.ethereal.email',
-          port: 587,
+      const transport = await nodemailer.createTransport(mg(
+        {
           auth: {
-              user: 'kyzpujmyyy36js4c@ethereal.email', // generated ethereal user
-              pass: 'yqasrcfMQ7aX5X4ujM'  // generated ethereal password
+            api_key: 'key-e14260ce2d73f3f009bef796dfbacbcd',
+            domain: 'mg.netslap.me'
           }
-      })
+        }))
       const info = await transport.sendMail(mail)
       Promise.resolve(info)
     }  catch(err) {
