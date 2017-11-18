daf3f226f48ddb17461da46fad51202f6ce1d985
Charles <cgarzon132@gmail.com>
2017-11-18 08:59:55 -0600

    Added middleware for async errors

---
+++ routes/asyncmid.js
@@ -0,0 +1,10 @@
+function asyncMiddleware(fn) {
+  return (req, res, next) => {
+    const routePromise = fn(req, res, next);
+    if (routePromise.catch) {
+      routePromise.catch(err => next(err));
+    }
+  }
+}
+
+module.exports = asyncMiddleware
