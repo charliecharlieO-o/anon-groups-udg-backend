aba8d4720a48121100f826ee318297d957abe579
Charles <cgarzon132@gmail.com>
2017-11-25 14:14:04 -0600

    Preliminary changes on settings.js to async await routes

--- routes/settings.js
+++ routes/settings.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const passport = require('passport')
 const router = express.Router()
+const asyncMid = require('./asyncmid') // Async
 
 const settings = require('../config/settings') //System settings
 const utils = require('../config/utils') //System utils
@@ -9,27 +10,26 @@ const utils = require('../config/utils') //System utils
 require('../config/passport')(passport)
 
 /* GET's the priviledge list for an admin */
-router.get('/priviledge/list', passport.authenticate('jwt', {'session': false}), (req, res) => {
+router.get('/priviledge/list', passport.authenticate('jwt', {'session': false}), asyncMid(async (req, res, next) => {
   if(utils.hasRequiredPriviledges(req.user.data.priviledges, ['promote_user', 'admin_admins'])){
     res.json({ 'success': true, 'doc': settings.priviledges })
+  } else  {
+    res.status(403).send('Unauthorized')
   }
-  else{
-    res.status(401).send('Unauthorized')
-  }
-})
+}))
 
 /* GET's the issue category list */
-router.get('/issue/categories', passport.authenticate('jwt', {'session': false}), (req, res) => {
+router.get('/issue/categories', passport.authenticate('jwt', {'session': false}), asyncMid(async (req, res, next) => {
   res.json({ 'success': true, 'doc': settings.issue_categories })
-})
+}))
 
 /* GET's general thread settings */
-router.get('/thread/settings', passport.authenticate('jwt', {'session': false}), (req, res) => {
+router.get('/thread/settings', passport.authenticate('jwt', {'session': false}), asyncMid(async (req, res, next) => {
   res.json({ 'success': true,
     'doc': {
       'maxThreadReplies': settings.max_thread_replies,
       'maxReplySubreplies': settings.max_reply_subreplies }
   })
-})
+}))
 
 module.exports = router
