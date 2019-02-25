03bef1e99b9d9deaba1cb187afd80ccf7adc99db
Charles <cgarzon132@gmail.com>
2019-02-25 11:45:23 -0600

    Update s3.js

--- config/s3.js
+++ config/s3.js
@@ -1,9 +1,9 @@
 const aws = require('aws-sdk')
 
 const s3 = new aws.S3({
-  endpoint: 'nyc3.digitaloceanspaces.com',
-  accessKeyId: 'QXC5NBOESTUHKZO4RPMI',
-  secretAccessKey: 'fjb3qo6M606+gjl5JGbkfE8QFZ7jbIucFhj2CGAi9cI'
+  endpoint: '',
+  accessKeyId: '',
+  secretAccessKey: ''
 })
 
 module.exports = s3
